997f43590c17041866d944fa29435d99317d0ac3
shaversj <shaversj@gmail.com>
2020-08-01 16:07:22 -0400

    Day 39 accomplishments!

--- days/39/rotation-py/solution.py
+++ days/39/rotation-py/solution.py
@@ -13,5 +13,28 @@ def is_rotation(word, rotated_word):
 
     return False
 
-print(is_rotation("Hello", "lloHe"))
-print(is_rotation("Basefont", "tBasefon"))
+# print(is_rotation("Hello", "lloHe"))
+# print(is_rotation("Basefont", "tBasefon"))
+
+
+def rotate_left(data, k):
+    # [10, 20, 30]
+    # rotate to the right by 1: [20, 30, 10]
+    l = len(data)
+    for i in range(0, k):
+        temp = data[0]
+        print(i)
+        for num in range(0, l - 1):
+            #print(num)
+            data[num] = data[num + 1]
+            print(data)
+
+        data[l - 1] = temp
+
+    return data
+
+
+print(rotate_left([10, 20, 30], 1))
+
+
+
