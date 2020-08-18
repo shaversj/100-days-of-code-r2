16ce040fe7a863172f8773099df1524ff6e0b999
shaversj <shaversj@gmail.com>
2020-08-17 20:34:54 -0400

    Day 47 accomplishments!

---
+++ days/46/tictactoe-py/tictactoe-game.py
@@ -0,0 +1,22 @@
+class TicTacToe:
+    def __init__(self):
+        board = Board()
+
+    def play_game(self):
+        pass
+
+    def take_one_turn(self):
+        pass
+
+
+
+class Board:
+    pass
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+    def take_turn(self):
+        pass
